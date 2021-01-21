30b75b2c2d1d06ed8fa1a332982508f239bb58a5
ar00gula <arianna.mayer@gmail.com>
2021-01-21 16:45:07 -0500

    commit tree 556490b4cbca23730d839eb9f8fc50eab48ad8f4

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'-' ']+/g, '')
+  }
+
+  static titleize(string) {
+    let array = string.split(" ")
+    let capString = array.map(function(element) {
+      
+      if (element != 'the' && element != 'a' && element != 'an' && element!= 'but' && element != 'of' && element != 'and' && element != 'for' && element != 'at' && element != 'by' && element != 'from') {
+        return Formatter.capitalize(element)
+      } else {
+        return element
+      }
+    }).join(" ")
+    return Formatter.capitalize(capString)
+  }
 }
